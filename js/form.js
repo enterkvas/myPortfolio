bd487093b6f4d4f026cd69bafaf2eec347acc16a
enterkvas <alexanderbugakov66@gmail.com>
2023-11-15 16:05:30 -0300

    15: The validation of the form is ready.

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 "use strict"
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
   const form = document.getElementById('form');
   form.addEventListener('submit', formSend);
 
@@ -8,11 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let error = formValidate(form);
 
+    // Sending the form by email:
     let formData = new FormData(form);
 
     if(error === 0){
       form.classList.add('_sending');
-      // let response = await fetch('sendmail.php', {
+      // let response = await fetch('mail.php', {
       //   method: 'POST', 
       //   body: formData
       // });
@@ -25,13 +26,51 @@ document.addEventListener('DOMContentLoaded', function() {
       //   alert('Error');
       //   form.classList.remove('_sending');
       // }
+    // //End of sending the form by email
     } else {
       alert('Fill in required fields!');
     }
   }
-
+  
   function formValidate(form){
     let error = 0;
+
+    // Phone number verification function:    
+    const inputTel = document.querySelector('.form__tel');
+    const errorLabel = document.createElement('label');
+    
+    function formAddErrorForTel(inputTel, text){
+      const parentTel = inputTel.parentNode;
+      parentTel.classList.add('errorTel');
+      inputTel.classList.add('_error');
+      errorLabel.classList.add('form__label');
+      errorLabel.textContent = text;
+      parentTel.append(errorLabel);
+    }
+
+    function formRemoveErrorForTel(inputTel){
+      const parentTel = inputTel.parentNode;
+      if(parentTel.classList.contains('errorTel')){
+        parentTel.classList.remove('errorTel');
+        parentTel.querySelector('label').remove();
+        inputTel.classList.remove('_error');
+      }
+    }
+    // Phone number testing function:
+    function phoneTest(inputTel){
+      return !/^[\d\+][\d\(\)\ -]{4,14}\d$/.test(inputTel.value);
+    }
+    // // End of phone number testing function
+    if(inputTel.value !== ''){
+      formRemoveErrorForTel(inputTel);
+      if(phoneTest(inputTel)){
+        formAddErrorForTel(inputTel, 'The first character must be a number or a plus. The last character is a number only. Parentheses, spaces and hyphens are allowed. From 4 to 14 characters in total.');
+        error++;      
+      }
+    }    
+    // // End of phone number verification function
+
+    // Checking that required fields are complete:
     let formReq = document.querySelectorAll('._req');
 
     for(let index = 0; index < formReq.length; index++) {
@@ -41,35 +80,38 @@ document.addEventListener('DOMContentLoaded', function() {
       if(input.value === '') {
         formAddError(input, 'Fill in required fields!');
         error++;
-      } else if(input.classList.contains('form__email')) {
-        // if(emailTest(input)){
-          formAddError(input, 'dkfdl');          
-          error++;
-        // };
-      } 
+      
+      } else {
+        if(input.classList.contains('form__email')){
+          if(emailTest(input)){         
+            error++;
+          }      
+        }
+      }      
     }
     return error;
   }
-  function formAddError(input, text) {
-    // input.parentElement.classList.add('_error');
+  // // End of checking that required fields are filled in
+
+  function formAddError(input, text){
     input.classList.add('_error');
     const formItems = input.parentNode;
     const errorLabel = document.createElement('label');
-    errorLabel.classList.add('error-label');
-    errorLabel.textContent = text;    
-    // input.classList.add('_error');
+    errorLabel.classList.add('form__label');
+    errorLabel.textContent = text;        
     formItems.append(errorLabel);
   }
-  function formRemoveError(input) {
-    // input.parentElement.classList.remove('_error');
+
+  function formRemoveError(input){
     const formItems = input.parentNode;
     if(input.classList.contains('_error')){
-      formItems.querySelector('.error-label').remove();
-    input.classList.remove('_error');
+      formItems.querySelector('.form__label').remove();
+      input.classList.remove('_error');
     }    
   }
-  // Email test function
-  function emailTest(input) {
+  // Email test function:
+  function emailTest(input){
     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
   }
+  // // End of email testing feature  
 });
--- js/script.js
+++ js/script.js
@@ -3,35 +3,35 @@
 const iconBurger = document.querySelector('.burger-btn'),
   // mouseScroll = document.querySelector('.mouse__scroll'),  
   navList = document.querySelector('.menu-list');
-iconBurger.addEventListener("click", function(e) {
+iconBurger.addEventListener("click", function(e){
   iconBurger.classList.toggle('_active');
   navList.classList.toggle('_active');
   // window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 // Burger Menu Link Items /Smooth Scrolling
 const menuLinks = document.querySelectorAll('.menu__link');
-if (menuLinks.length > 0) {
+if (menuLinks.length > 0){
 	menuLinks.forEach(menuLink => {
 		menuLink.addEventListener("click", onMenuLinkClick);
 	});
-	function onMenuLinkClick(e) {
+	function onMenuLinkClick(e){
 		const menuLink = e.target;
 		const w = document.body.clientWidth;
-		if (w < 992) {
+		if (w < 992){
 			navList.classList.toggle('_active');			
       iconBurger.classList.toggle('_active');		
-			if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
 				const gotoBlock = document.querySelector(menuLink.dataset.goto);
 				const gotoBlockValue = gotoBlock.getBoundingClientRect().top;				
 				window.scrollTo ({top: gotoBlockValue,	behavior: "smooth"});
 			}			
-		} else if (w > 991) {			
-			if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {				
+		} else if (w > 991){			
+			if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){				
 				const gotoBlock = document.querySelector(menuLink.dataset.goto);
-				if (window.pageYOffset === 0) {
+				if (window.pageYOffset === 0){
 					const gotoBlockValue = gotoBlock.getBoundingClientRect().top;	
 					window.scrollTo ({top: gotoBlockValue,	behavior: "smooth"});				
-				} else if (window.pageYOffset > 0) {
+				} else if (window.pageYOffset > 0){
 					const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
 					window.scrollTo ({top: gotoBlockValue,	behavior: "smooth"});				
 				}				
@@ -42,8 +42,8 @@ if (menuLinks.length > 0) {
 };
 //mouse__scroll/Smooth Scrolling
 const mouseScroll = document.querySelector('.mouse__scroll');
-mouseScroll.addEventListener('click', function(e) {  
-  if (mouseScroll.dataset.mouse && document.querySelector(mouseScroll.dataset.mouse)) {
+mouseScroll.addEventListener('click', function(e){  
+  if (mouseScroll.dataset.mouse && document.querySelector(mouseScroll.dataset.mouse)){
     const mouseBlock = document.querySelector(mouseScroll.dataset.mouse);
     const mouseBlockValue = mouseBlock.getBoundingClientRect().top;				
     window.scrollTo ({top: mouseBlockValue,	behavior: "smooth"});
@@ -52,22 +52,22 @@ mouseScroll.addEventListener('click', function(e) {
   e.preventDefault();   
 }); 
 // =========== Button Up =======
-function btn_up() {
+function btn_up(){
   const btn_upElement = document.createElement('div');
   btn_upElement.className = 'btn_up';
   document.body.append(btn_upElement);
 
-  btn_upElement.addEventListener('click', function (e) {    
+  btn_upElement.addEventListener('click', function (e){    
     window.scrollTo({ top: 0, behavior: 'smooth' });
     btn_upElement.classList.toggle('active');
     e.preventDefault();
   });
-  function trackScroll() {
+  function trackScroll(){
     const scrolled = window.pageYOffset;
     const coords = document.documentElement.clientHeight;
 
-    if (scrolled > coords) { btn_upElement.classList.add('active'); }
-    if (scrolled < coords) { btn_upElement.classList.remove('active'); }
+    if (scrolled > coords){ btn_upElement.classList.add('active'); }
+    if (scrolled < coords){ btn_upElement.classList.remove('active'); }
   }
   window.addEventListener('scroll', trackScroll);
 }
@@ -135,17 +135,17 @@ const list = document.querySelector('#categories'),
   blocks = document.querySelectorAll('.card'),
   filters = list.querySelectorAll('.filter');
 list.addEventListener('click', e => {
-  if(e.target.classList.contains('filter')) {
+  if(e.target.classList.contains('filter')){
     const filter = e.target.dataset.id;
-    if(filter === 'all') {
+    if(filter === 'all'){
       blocks.forEach(block => {
         block.style.display = 'block';
       }); 
-    } else {
+    } else{
         blocks.forEach(block => {
-          if(block.classList.contains(filter)) {
+          if(block.classList.contains(filter)){
             block.style.display = 'block';
-          } else {
+          } else{
             block.style.display = 'none';
           }
         });
