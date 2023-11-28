b00d48e2508a2b9d1d6c006bf98f180c17324ce0
enterkvas <alexanderbugakov66@gmail.com>
2023-11-27 23:34:42 -0300

    19: The resume section for 768 is ready.

--- js/form.js
+++ js/form.js
@@ -3,13 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
   const form = document.getElementById('form');
   form.addEventListener('submit', formSend);
 //------------------------------------------------
-// Function for submitting the entire form:
+  // Function for submitting the entire form:
   async function formSend(e){
     e.preventDefault();
 
     let error = formValidate(form);
 
-  // Sending the form by email:
     let formData = new FormData(form);
 
     if(error === 0){
@@ -32,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function(){
       alert('Fill in required fields!');
     }
   }  
-//-----------------------------------------------
+  //-----------------------------------------------
   // Function for validation:  
   function formValidate(form){
     let error = 0;
@@ -54,19 +53,10 @@ document.addEventListener('DOMContentLoaded', function(){
       } else if(input.classList.contains('form__email')){        
         if(emailTest(input)){         
           error++;
-        }      
-        // if(input.classList.contains('form__tel')){        
-        //   if(phoneTest(input)){         
-        //     error++;
-        //   }
-        // }      
-      }         
-        // if(input.classList.contains('form__tel')){        
-        //   if(emailTest(input)){         
-        //     error++;
-        //   }      
-        // }            
+        }           
+      }                     
     }
+    //----------------------------------------
 
     function formAddErrorForTel(inputTel, text){
       const parentTel = inputTel.parentNode;
@@ -83,11 +73,6 @@ document.addEventListener('DOMContentLoaded', function(){
         parentTel.querySelector('.form__label').remove();
         inputTel.classList.remove('_error');
       }
-      // parentTel.classList.add('errorTel');
-      // inputTel.classList.add('_error');
-      // errorLabel.classList.add('form__label');
-      // errorLabel.textContent = text;
-      // parentTel.append(errorLabel);
     }
     // Phone number testing function:
     function phoneTest(inputTel){
@@ -96,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     if(inputTel.value !==''){
       if(phoneTest(inputTel)){
-        formAddErrorForTel(inputTel, 'No one!');
+        formAddErrorForTel(inputTel, 'The first character must be a number or a plus. The last character is a number only. Parentheses, spaces and hyphens are allowed. From 4 to 14 characters in total.');
         error++;
       } else {
         formRemoveErrorForTel(inputTel);
@@ -104,10 +89,8 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     return error;
-  }
-// // End of checking that required fields are filled in
-  
-
+  }  
+  //-------------------------------------------------
   function formAddError(input, text){
     input.classList.add('_error');
     const formItems = input.parentNode;
@@ -128,33 +111,5 @@ document.addEventListener('DOMContentLoaded', function(){
   function emailTest(input){
     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
   }
-
-  
-  // // End of email testing feature  
-
-  //+++++++++ Temporarily not in use: +++++++++++++++++++++++
-    // Phone number verification function:    
-    
-    // // End of phone number testing function
-
-    // if(inputTel.value ===''){
-    //   formAddErrorForTel(inputTel, 'Fill in required fields!');
-    //   error++;
-    // } else if(phoneTest(inputTel)){
-    //   formRemoveErrorForTel(inputTel);
-    //   error++;
-    // }
-    // }
-  
-
-
-    // if(inputTel.value !== ''){
-    //   formRemoveErrorForTel(inputTel);
-    //   if(phoneTest(inputTel)){
-    //     formAddErrorForTel(inputTel, 'The first character must be a number or a plus. The last character is a number only. Parentheses, spaces and hyphens are allowed. From 4 to 14 characters in total.');
-    //     error++;      
-    //   }
-    // }    
-    // // End of phone number verification function
-//+++++++++ //Temporarily not in use +++++++++++++++++++++++
+  //-------------------------------
 });
