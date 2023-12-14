266584b68437b463e2efb9271370e741451338a7
enterkvas <alexanderbugakov66@gmail.com>
2023-12-14 16:18:48 -0300

    25: Before testing adaptive fonts

--- js/form.js
+++ js/form.js
@@ -112,5 +112,4 @@ document.addEventListener('DOMContentLoaded', function(){
   function emailTest(input){
     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
   }
-  //-------------------------------
 });
